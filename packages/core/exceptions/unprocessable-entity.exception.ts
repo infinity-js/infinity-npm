189764fe0269472338b715120dd2fd4e66d607a5
White Angel <edmilsonsw22@gmail.com>
2022-09-16 16:40:20 -0400

    feat(core): add unprocessable-entity exception

---
+++ packages/core/exceptions/unprocessable-entity.exception.ts
@@ -0,0 +1,20 @@
+import { HttpStatus, RpcStatus } from 'enums';
+import { InfinityGlobalException } from './global.exception';
+
+type UnprocessableEntityExceptionParamsDTO = {
+  entity: string;
+  message: string;
+  portugueseMessage: string;
+};
+
+export class UnprocessableEntityException extends InfinityGlobalException {
+  constructor(params: UnprocessableEntityExceptionParamsDTO) {
+    super({
+      name: `UnprocessableEntityException (${params.entity})`,
+      message: params.message,
+      portugueseMessage: params.portugueseMessage,
+      rpcCode: RpcStatus.INVALID_ARGUMENT,
+      statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    });
+  }
+}
