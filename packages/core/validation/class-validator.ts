c3b26f713822200bcd70e77f0731623a82e5c284
White Angel <edmilsonsw22@gmail.com>
2022-09-16 16:40:48 -0400

    feat(core): create validate-entity function

---
+++ packages/core/validation/class-validator.ts
@@ -0,0 +1,52 @@
+import { plainToInstance } from 'class-transformer';
+import { validateSync, ValidationError } from 'class-validator';
+import { UnprocessableEntityException } from 'exceptions';
+
+function dfs(
+  src: ValidationError[],
+  dest: Constraint[],
+  msg: string | null = null,
+) {
+  const cur = msg ? `${msg}` : '';
+  for (const validationError of src) {
+    if (validationError.constraints) {
+      for (const key in validationError.constraints) {
+        if (validationError.constraints[key]) {
+          const formatProperty = cur
+            ? `.${validationError.property}`
+            : `${validationError.property}`;
+          const field = isNaN(parseInt(validationError.property, 10))
+            ? formatProperty
+            : `[${validationError.property}]`;
+          dest.push({
+            field: `${cur}${field}`,
+            message: `${validationError.constraints[key]}`,
+          });
+        }
+      }
+    }
+
+    if (validationError.children && validationError.children.length) {
+      const formatProperty = cur
+        ? `.${validationError.property}`
+        : `${validationError.property}`;
+      const field = isNaN(parseInt(validationError.property, 10))
+        ? formatProperty
+        : `[${validationError.property}]`;
+      dfs(validationError.children, dest, `${cur}${field}`);
+    }
+  }
+}
+
+interface Constraint {
+  field: string;
+  message: string;
+}
+
+export class MessageFormatter {
+  public static format(validationErrors: ValidationError[]) {
+    const dest: Constraint[] = [];
+    dfs(validationErrors, dest);
+    return dest;
+  }
+}
---
+++ packages/core/validation/validate-entity.ts
@@ -0,0 +1,29 @@
+import { plainToInstance } from 'class-transformer';
+import { validateSync } from 'class-validator';
+import { UnprocessableEntityException } from 'exceptions';
+import { MessageFormatter } from './class-validator';
+
+export function validateEntity<T extends object>(
+  classValue: new () => T,
+  data: any,
+) {
+  if (!data) {
+    throw new UnprocessableEntityException({
+      entity: classValue.name,
+      message: 'Data is required',
+      portugueseMessage: 'Dados são obrigatórios',
+    });
+  }
+
+  const dtoToValidate = plainToInstance(classValue, data);
+
+  const errors = validateSync(dtoToValidate);
+
+  if (errors.length > 0) {
+    throw new UnprocessableEntityException({
+      entity: classValue.name,
+      message: JSON.stringify(MessageFormatter.format(errors)),
+      portugueseMessage: 'Falha na validação',
+    });
+  }
+}
